10239c48a65d74caf19a51c7eb0e2aa784bfcba3
Raksha005 <155376011+Raksha005@users.noreply.github.com>
2024-03-26 20:06:07 +0530

    added py file

---
+++ first.py
@@ -0,0 +1,21 @@
+#Code for language translator!
+from googletrans import Translator 
+
+def translate_to_hindi(text):
+    translator = Translator() 
+    translated_text = translator.translate(text, src='en', dest='hi')
+    return translated_text.text
+
+def main():
+    print("Welcome to English to Hindi Translator")
+    while True:
+        user_input = input("Enter text in English (type 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            print("Thank you for using the translator!")
+            break
+        else:
+            translated_text = translate_to_hindi(user_input)
+            print("Translated Text (Hindi):", translated_text)
+
+if __name__ == "__main__":
+    main()
